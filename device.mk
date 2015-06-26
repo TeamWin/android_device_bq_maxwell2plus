577fcd9560b5d703fbcc1e0fd34fca09db9dde3a
Kra1o5 <kra1o5x@gmail.com>
2015-06-26 14:28:26 +0200

    Fix encryption & copy kernel
    Change-Id: I7fd1b5846f4fffef75489cd71132ffe1a026533c

--- device.mk
+++ device.mk
@@ -7,6 +7,7 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/kernel:root/kernel \
     $(LOCAL_PATH)/prebuilt/init:root/init \
     $(LOCAL_PATH)/prebuilt/rk30xxnand_ko.ko.3.0.36+:root/rk30xxnand_ko.ko.3.0.36+ \
     $(LOCAL_PATH)/init.recovery.rk30board.rc:root/init.recovery.rk30board.rc
